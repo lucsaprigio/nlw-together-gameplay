9510214ca5567ad5fa02c8de9eb03da90d84a093
Aprigio <aprigio@pop-os.localdomain>
2021-06-22 21:45:55 -0300

    Categories

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,12 +4,21 @@ import {
     Text,
     Image,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { styles } from './styles';
+import IllustrationImg from '../../assets/illustration.png';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
-import IllustrationImg from '../../assets/illustration.png';
-import { styles } from './styles';
+
 
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
     return (
         <View style={styles.container}>
             <Image 
@@ -30,7 +39,9 @@ export function SignIn() {
                 </Text>
 
                 <ButtonIcon 
-                    title="Entrar com Discord" 
+                    title="Entrar com Discord"
+                    activeOpacity={0.7} 
+                    onPress={handleSignIn}
                 />
             </View>
         </View>
