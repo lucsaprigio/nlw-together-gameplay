a73283088d8f5b44b308ce8f6fa5df73f35afb04
Aprigio <aprigio@pop-os.localdomain>
2021-06-25 20:13:12 -0300

    Appointment Createa and modal screen

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -5,12 +5,20 @@ import { Feather } from '@expo/vector-icons';
 import { 
     Text,
     View,
+    ScrollView,
+    Platform,
+    KeyboardAvoidingView,
 } from 'react-native';
 
-import  { Background } from '../../components/Background';
 import  { CategorySelect } from '../../components/CategorySelect';
 import  { Header } from '../../components/Header';
 import  { GuildIcon } from '../../components/GuildIcon';
+import  { SmallInput } from '../../components/SmallInput';
+import  { TextArea } from '../../components/TextArea';
+import  { Button } from '../../components/Button';
+import  { ModalView } from '../../components/ModalView';
+import  { Guilds } from '../Guilds';
+import { GuildProps } from '../../components/Guild';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
@@ -18,9 +26,25 @@ import { styles } from './styles';
 
 export function AppointmentCreate() {
     const [category, setCategory] = useState('');
+    const [openGuildsModal, setOpenGuildsModal] = useState(false);
+    const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+    function handleOpenGuilds() {
+        setOpenGuildsModal(true);
+    }
+
+    function handleGuildSelect(guildSelected: GuildProps) {
+        setGuild(guildSelected);
+        setOpenGuildsModal(false);
+    }
 
     return (
-        <Background>
+    <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+    >
+    
+        <ScrollView> 
             <Header 
                 title="Agendar Partida"
             />
@@ -38,15 +62,16 @@ export function AppointmentCreate() {
             />
 
             <View style={styles.form}>
-                <RectButton>
+                <RectButton
+                onPress={handleOpenGuilds}>
                     <View style={styles.select}>
-                        {/* <View style={styles.image}/> */
-                        <GuildIcon />
+                        { 
+                        guild.icon ? <GuildIcon /> : <View style={styles.image} /> 
                         }
                         
                         <View style={styles.selectBody}>
                             <Text style={styles.label}>
-                                Selecione um servidor
+                                {guild.name ? guild.name : 'Selecione um servidor'}
                             </Text>
                         </View>
 
@@ -57,7 +82,64 @@ export function AppointmentCreate() {
                         />
                     </View>
                 </RectButton>
+                
+                <View style={styles.field}>
+                    <View>
+                        <Text style={styles.label}>
+                            Dia e mês
+                        </Text>
+
+                        <View style={styles.column}>
+                        <SmallInput  maxLength={2} />
+                        <Text style={styles.divider}>
+                            /
+                        </Text>
+                        <SmallInput  maxLength={2} />
+                    </View>        
+                   </View>
+
+                   <View>
+                        <Text style={styles.label}>
+                            Hora e minuto
+                        </Text>
+
+                        <View style={styles.column}>
+                        <SmallInput  maxLength={2} />
+                        <Text style={styles.divider}>
+                            :
+                        </Text>
+                        <SmallInput  maxLength={2} />
+                    </View>        
+                   </View>
+                </View>
+
+                <View style={[styles.field, { marginBottom: 12 }]}>
+                    <Text style={styles.label}>
+                        Descrição
+                    </Text>
+
+                    <Text style={styles.caracteresLimit}>
+                        Max 100 caracteres
+                    </Text>
+                </View>
+
+                <TextArea
+                    multiline
+                    maxLength={100}
+                    numberOfLines={5}
+                    autoCorrect={false}
+                />
+
+                <View style={styles.footer}>
+                    <Button title="Agendar" />
+                </View>
             </View>
-    </Background>
+        </ScrollView>
+
+        <ModalView visible={openGuildsModal}>
+            <Guilds handleGuildSelect={handleGuildSelect}/>
+        </ModalView>
+
+    </KeyboardAvoidingView>
     );
 }
