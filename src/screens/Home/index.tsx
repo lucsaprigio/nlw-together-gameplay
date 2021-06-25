1211c40069bf5fd2fc2b074bc325cf75bed9753d
Aprigio <aprigio@pop-os.localdomain>
2021-06-24 23:07:47 -0300

    app

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View , FlatList } from 'react-native';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
+import { CategorySelect } from '../../components/CategorySelect';
+import { Background } from '../../components/Background';
+
+import { styles } from './styles';
+
+export function Home() {
+    const [category, setCategory] = useState('');
+
+    const navigation = useNavigation();
+
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger.'
+        },
+        {
+            id: '2',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger.'
+        }
+    ]
+
+    function handleCategorySelect(categoryId: string) {
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    };
+
+    function handleAppointmentDetails() {
+        navigation.navigate('AppointmentDetails')
+    }
+
+    function handleAppointmentCreate() {
+        navigation.navigate('AppointmentCreate')
+    }
+
+    return (
+    <Background>
+        <View>
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd onPress={handleAppointmentCreate}/>
+            </View>
+
+                <CategorySelect 
+                    categorySelected={category}
+                    setCategory={handleCategorySelect}
+                />
+
+                <View style={styles.content}>
+                    <ListHeader 
+                        title="Partidas agendadas"
+                        subtitle="Total 6"
+                    />
+
+                    <FlatList 
+                        data={appointments}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <Appointment 
+                                data={item}
+                                onPress={handleAppointmentDetails} 
+                            />
+                        )}
+                        ItemSeparatorComponent={() => <ListDivider />}
+                        style={styles.matches}
+                        showsVerticalScrollIndicator={false}
+                    />
+                </View>
+        </View>
+    </Background>
+    )
+}
